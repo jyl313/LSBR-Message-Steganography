f7fed02e79c135d58ff5f0bcd9a1243083815e6c
Jia Yi Loo <loo_jia_yi@i2r.a-star.edu.sg>
2025-12-03 16:32:59 +0800

    [DOCS] Add docstring and more comments

--- src/lsbr_msg.py
+++ src/lsbr_msg.py
@@ -1,49 +1,109 @@
 from PIL import Image
 import numpy as np
 
-# Load image as flatten numpy array
-def load_image(img):
+def load_image(img: str) -> tuple[tuple, np.ndarray]:
+    """
+    Load image as flatten numpy array.
+
+    Args:
+        img (str):  Path to image file
+
+    Returns:
+        out1 (tuple):       Shape of the image; (width, height).
+        out2 (np.ndarray]): Flatten image pixels into 1D.
+    """
     img = Image.open(img)
     arr = np.array(img)
     return arr.shape, arr.flatten()
 
-# Convert message to binary
-def convert_msg(msg):
-    msg = msg
+
+def convert_msg(msg: str) -> str:
+    """
+    Convert (each character in) message to binary strings.
+    E.g 'hello' -> '0110100001100101011011000110110001101111'.
+    Add '1' at the end- this indicates the end of message.
+    NOTE: Standard ASCII character has value from 0 to 127.
+    Binary of characters wouldn't start with 1 (since it cannot go to 128).
+
+    Args:
+        msg (str):  Message to be converted into binary.
+
+    Returns:
+        out (str):  Message converted into binary.
+    """
     binary_msg = ''
     for i in msg:
-        binary_msg+=(format(ord(i), '08b'))
+        binary_msg+= format(ord(i), '08b')
     binary_msg+='1'
     return binary_msg
 
-# Change last bit of image pixel to message bit
-def change_bit(img_pix, msg_bit):
+
+def change_bit(img_pix: str, msg_bit: str) -> int:
+    """
+    Change the last bit of image pixel (regardless if last bit
+    is the same as `msg_bit`.)
+
+    Args:
+        img_pix (str):  Image pixel (in binary string).
+        msg_bit (str):  Message bit (in binary string).
+
+    Returns:
+        out (int):  Updated image pixel after embedding message bit.
+    """
     new_pix = int(img_pix[:-1] + str(msg_bit), 2)
     return new_pix
 
-# Encode message to image
-def encode_msg(msg, img):
+
+def encode_msg(msg: str, img: str) -> Image.Image:
+    """
+    Encode message to image.
+
+    Args:
+        msg (str):  Message to be encoded to image.
+        img (str): Path to image file for encoding.
+
+    Returns:
+        out (Image.Image): Image with encoded message.
+    """
     binary_msg = convert_msg(msg)
     shape, img = load_image(img)
     # Hide message in last bit of image pixel
     for i in range(len(binary_msg)):
         img[i] = change_bit(format(img[i], '08b'), binary_msg[i])
     return Image.fromarray(np.reshape(img, shape))
-        
-# Decode encoded message in image    
-def decode_msg(img):
+
+
+def decode_msg(img: str) -> str:
+    """
+    Decode (any) message, that is encoded in image.
+    Stop decoding when met with binary that starts with '1'.
+    NOTE: Standard ASCII character has value from 0 to 127.
+    Binary of characters wouldn't start with 1 (since it cannot go to 128).    
+
+    Args:
+        img (str): Path to image file for decoding.
+    
+    Returns:
+        out (str): Decoded message.
+    """
     shape, img = load_image(img)
     extract_char = ''
     msg = ''
     for i in range(len(img)):
-        if len(extract_char)==8:
-            if extract_char[0] == '1':
-                break
-            msg+=chr(int(extract_char,2))
-            extract_char = ''
         extract_char+=bin(img[i])[-1]
+        # This indicates a non-ascii character.
+        # This is likely the '1' placed during encoding,
+        # to indicate end of message.
+        if len(extract_char)==1 and extract_char[0] == '1':
+            break
+        # Convert the 8-bit binary into character and append
+        if len(extract_char)==8:
+            msg+=chr(int(extract_char,2))
+            # Reset variable to get the 8-bit binary
+            extract_char = ''
     return msg
-        
+
+
 if __name__=="__main__":
     opt = input("1: Encode, 2: Decode\n")
     if opt == '1':
